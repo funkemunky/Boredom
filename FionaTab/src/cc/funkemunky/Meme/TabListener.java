37dbc0dee84232cf046815e0af54ef9ca5a8dcd0
funkemunky <funkemunky@users.noreply.github.com>
2017-12-23 20:35:48 -0500

    Another Commit

--- FionaTab/src/cc/funkemunky/Meme/TabListener.java
+++ FionaTab/src/cc/funkemunky/Meme/TabListener.java
@@ -10,8 +10,9 @@ import org.bukkit.entity.Player;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
 
-import anticheat.FionaAPI;
+import anticheat.Fiona;
 import anticheat.detections.Checks;
+import anticheat.user.User;
 import anticheat.utils.Color;
 import me.vertises.aztec.tablist.TablistEntrySupplier;
 
@@ -29,25 +30,26 @@ implements TablistEntrySupplier {
 		HashBasedTable<Integer, Integer, String> table = HashBasedTable.create();
 
 		DecimalFormat format = new DecimalFormat("#.##"); 
-		table.put(0, 3, Color.Yellow + Color.Bold + "Player Information");
-		table.put(0, 4, Color.Gray + "Ping: " + Color.White + FionaAPI.getAPI().getPing(player));
-		table.put(0, 5, Color.Gray + "Flight: " + Color.White + player.getAllowFlight());
-		table.put(0, 6, Color.Gray + "Op: " + Color.White + player.isOp());
-		table.put(1, 2, Color.Gold + Color.Bold + "Fiona Test Server");
-		table.put(1, 4, Color.Yellow + Color.Bold + "Your Violations");
-		int i = 5;
-		if (FionaAPI.getAPI().getViolations(player).size() > 0) {
-			for (Checks check : FionaAPI.getAPI().getViolations(player).keySet()) {
-				table.put(1, i, Color.White + "- " + Color.Gray + check.getName() + Color.Red + " (" + FionaAPI.getAPI().getViolations(player).get(check) + " VL)");
+		table.put(0, 1, Color.Gold + "Player Information");
+		table.put(0, 2, Color.Gray + "Ping: " + Color.White + Fiona.getAC().getPing().getPing(player));
+		table.put(0, 3, Color.Gray + "Flight: " + Color.White + player.getAllowFlight());
+		table.put(0, 4, Color.Gray + "Op: " + Color.White + player.isOp());
+		table.put(1, 0, Color.Gold + Color.Bold + "Fiona Test Server");
+		table.put(1, 3, Color.Yellow + Color.Bold + "Your Violations");
+		int i = 4;
+		User user = Fiona.getUserManager().getUser(player.getUniqueId());
+		if (user.getVLs().size() > 0) {
+			for (Checks check : user.getVLs().keySet()) {
+				table.put(1, i, Color.White + "- " + Color.Gray + check.getName() + Color.Red + " (" + user.getVLs().get(check) + " VL)");
 			    i++;
 			} 
 		} else {
-			table.put(1, 5, Color.Green + Color.Italics + "No violations recorded.");
+			table.put(1, 4, Color.Green + Color.Italics + "No violations recorded.");
 		}
-		table.put(2, 3, Color.Yellow + Color.Bold + "Fiona Information");
-		table.put(2, 4, Color.Gray + "Fiona Version: " + Color.White + FionaAPI.getAPI().getVersion());
-		table.put(2, 5, Color.Gray + "Alerts: " + (FionaAPI.getAPI().hasAlerts(player) ? Color.Green + FionaAPI.getAPI().hasAlerts(player) : Color.Red + FionaAPI.getAPI().hasAlerts(player)));
-		table.put(2, 6, Color.Gray + "TPS: " + Color.White + format.format(FionaAPI.getAPI().getTPS()));
+		table.put(2, 1, Color.Gold + "Fiona Information");
+		table.put(2, 2, Color.Gray + "Fiona Version: " + Color.White + plugin.getServer().getPluginManager().getPlugin("Fiona").getDescription().getVersion());
+		table.put(2, 3, Color.Gray + "Alerts: " + (user.isHasAlerts() ? Color.Green + user.isHasAlerts() : Color.Red + user.isHasAlerts()));
+		table.put(2, 4, Color.Gray + "TPS: " + Color.White + format.format(Fiona.getAC().getPing().getTPS()));
 		return table;
 	}
 
