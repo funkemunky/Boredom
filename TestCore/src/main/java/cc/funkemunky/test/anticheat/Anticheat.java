c3b908cc8bc44d48f72c25060c91b6e842c305d7
Dawson Hessler <funkemunkybiz@gmail.com>
2019-09-03 09:28:59 -0400

    new stuff

---
+++ TestCore/src/main/java/cc/funkemunky/test/anticheat/Anticheat.java
@@ -0,0 +1,11 @@
+package cc.funkemunky.test.anticheat;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public abstract class Anticheat {
+    public Map<UUID, String> lastViolation = new HashMap<>();
+
+
+}
--- TestCore/src/main/java/cc/funkemunky/test/utils/ConfigSettings.java
+++ TestCore/src/main/java/cc/funkemunky/test/utils/ConfigSettings.java
@@ -8,4 +8,10 @@ public class ConfigSettings {
 
     @ConfigSetting(name = "kauri-version")
     public static String kauriVersion = "b50";
+
+    @ConfigSetting(path = "scoreboard", name = "enabled")
+    public static boolean scoreboardEnabled = true;
+
+    @ConfigSetting(path = "scoreboard", name = "anticheat")
+    public static String anticheat = "Kauri";
 }
